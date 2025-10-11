6b94d3599b2d151787ec270afe8f3d72c2242dcc
Jayden Lewis <jaydent.lewis@gmail.com>
2025-10-11 17:01:31 -0400

    Completed Employee Model

--- models/Employee.js
+++ models/Employee.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
 
-    first_name: { type: String, required: true},
-    last_name: { type: String, required: true},
-    email:    { type: String, required: true},
-    password: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email:    { type: String, required: true },
+    position: { type: String, required: false },
+    salary: { type: Number, required: false },
+    date_of_joining: { type: Date, default: Date.now, required: true },
+    department: { type: String, required: false },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
 
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Employee', employeeSchema);
